503eda074b19a3d6fd3b27d8cc40426dda44a057
carlotacb <carkbra@gmail.com>
2019-02-02 20:53:05 +0100

    register

---
+++ client/travel-easy/src/components/Register/register.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { Button, FormGroup, FormControl } from "react-bootstrap";
+import './register.css';
+
+class Register extends Component {
+  
+    constructor(props) {
+      super(props);
+      this.state = {
+        register: props.register,
+        name: String,
+        username: String,
+        password: String,
+        email: String
+      };
+      this.submit = this.submit.bind(this);
+    }
+
+    submit() {
+        this.props.history.push("/home");
+    }
+
+    render() {
+        return (
+            <div className="Register">
+            
+            <h1>Sign Up in Travel Made Simple it's too simple</h1>
+
+                <form onSubmit={this.handleSubmit}>                    
+                    <FormGroup controlId="name" bsSize="large">
+                        <label>Name</label>
+                        <FormControl autoFocus value={this.state.name} onChange={this.handleChange}/>
+                    </FormGroup>
+
+                    <FormGroup controlId="username" bsSize="large">
+                        <label>Username</label>
+                        <FormControl autoFocus value={this.state.username} onChange={this.handleChange}/>
+                    </FormGroup>
+
+                    <FormGroup controlId="email" bsSize="large">
+                        <label>Email</label>
+                        <FormControl type="email" value={this.state.email} onChange={this.handleChange}/>
+                    </FormGroup>
+
+                    <FormGroup controlId="password" bsSize="large">
+                        <label>Password</label>
+                        <FormControl value={this.state.password} onChange={this.handleChange} type="password" />
+                    </FormGroup>
+                    
+                    <Button block bsSize="large" onClick = {this.submit} type="submit">Start planning!</Button>
+                </form>
+            </div>
+        );
+      }
+    }
+
+export default Register;
